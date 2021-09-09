9040955178e59ec47c90f510522f4ed0e7ecd61a
Adam Light <alight@coloradocollege.edu>
2021-09-09 15:38:32 -0600

    Finished quiz w/ errorbars

--- q1_in_class.py
+++ q1_in_class.py
@@ -15,4 +15,15 @@ n_points = CO2_ppm_MaunaLoa.size
 days = np.arange(n_points)*14
 years = days/365.0 + 1981
 
-plt.plot(years, CO2_ppm_MaunaLoa)
+CO2_unc = 5.0
+#plt.plot(years, CO2_ppm_MaunaLoa)
+plt.errorbar(years, CO2_ppm_MaunaLoa, yerr=5.0, errorevery=20)
+
+fit, cov = np.polyfit(years, CO2_ppm_MaunaLoa, 1, cov=True)
+
+fit_func = np.poly1d(fit)
+plt.errorbar(years, fit_func(years), yerr=np.sqrt(cov[0,0]), errorevery=10)
+
+plt.xlabel("Year")
+plt.ylabel("$CO_2$ Concentration (ppm)")
+plt.title("Mauna Loa $CO_2$ concentration increases at {:.2f} ppm/yr".format(fit[0]))
